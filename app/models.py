032b3b4760ba46ee97bb389f9e23337406648846
EdgarRetes <edgar.retes24@gmail.com>
2024-11-13 14:44:27 -0600

    [ADD] models and csv reader

--- app/models.py
+++ app/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+    
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
+    
+class Matrix(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return "Matrix" + self.user_id
 
-# Create your models here.
+class Neuron(models.Model):
+    color = models.CharField(max_length=100)
+    size = models.FloatField()
+    opacity = models.FloatField()
+    neuron_no = models.IntegerField()
+    matrix_id = models.ForeignKey(Matrix, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return "Neuron" + self
+    
+class Conection(models.Model):
+    con_neuron_id = models.ForeignKey(Neuron, related_name="con_neuron_connections", on_delete=models.CASCADE)
+    neuron_id = models.ForeignKey(Neuron, related_name="neuron_connections", on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.neuron_id + self.con_neuron_id
+    
+    
--- app/views.py
+++ app/views.py
@@ -1,5 +1,24 @@
+import csv
+from io import StringIO
 from django.shortcuts import render
-
-# Create your views here.
+from django.http import HttpResponse
+from .models import * 
 def home(request):
+    if request.method == 'POST' and request.FILES['file']:
+        csv_file = request.FILES['file']
+        
+        try:
+            csv_file_content = csv_file.read().decode('utf-8-sig')  
+        except UnicodeDecodeError:
+            csv_file_content = csv_file.read().decode('utf-8')
+        
+        csv_data = StringIO(csv_file_content)
+        reader = csv.reader(csv_data)
+        
+        for i, row in enumerate(reader):
+            for j in range(len(row)):
+                value = float(row[j])
+                print(f"Coordenadas (i={i}, j={j}): {value}")
+        
+        return render(request, 'home.html', {'message': 'Archivo procesado correctamente'})
     return render(request, 'home.html')
