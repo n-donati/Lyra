9b8f3865a1e96f25cdd24a1528070937ce004056
Nicolas Donati <112676468+n-donati@users.noreply.github.com>
2024-11-14 05:47:35 -0600

    fix

--- lyra/settings.py
+++ lyra/settings.py
@@ -12,10 +12,6 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
-from decouple import config
-
-OPENAI_API_KEY = config('OPENAI_API_KEY')
-
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
